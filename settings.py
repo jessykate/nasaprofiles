42d02b28f8366f7f793545700528eb2c3be1c0ca
Jessy Cowan-Sharp <jessy.cowansharp@gmail.com>
2009-12-24 05:49:39 +0000

    updated readme and settings.example.py comments to be more verbose

--- settings.py
+++ settings.py
@@ -1,13 +1,17 @@
 from database import Database
 import os
 settings = {
-    'db':Database().connect(),
-    "static_path": os.path.join(os.path.dirname(__file__), "static"),
-    'domain': 'localhost',
+    # what domain is your app running at?
+    'domain': 'people.opennasa.com',
     'port': 8989,
     'email_enabled': False,
     'debug': True,
     'smtp_user': 'people@opennasa.com',
     'smtp_pass': '',
+
+    # you should probably stop editing here unless you know what
+    # you're doing
+    'db':Database().connect(),
+    "static_path": os.path.join(os.path.dirname(__file__), "static"),
     'login_url': '/',
 }
